6de343a342e0be9fabe13e8a514e764a44a3c065
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-02-13 23:45:01 +0530

    feat(welcome): basic username and display name input

---
+++ app/(auth)/welcome/page.tsx
@@ -0,0 +1,39 @@
+import WelcomePageForm from "@/components/auth/welcome-page-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+function WelcomeScreen() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-black p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
+
+function LoginForm() {
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome</CardTitle>
+          <CardDescription>Just some quick setup</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <WelcomePageForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default WelcomeScreen;
